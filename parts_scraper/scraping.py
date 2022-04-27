f8fa42783f8028022e3dce60d5ffbb8de3e440f2
stevanthomas <stevan.thomas@blend360.com>
2022-04-27 14:28:38 -0400

    Created preliminary dataframe

--- parts_scraper/scraping.py
+++ parts_scraper/scraping.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import inquirer
 import re
+import pandas as pd
 
 #Getting the base URL from rockauto. All searches in English start with this
 url="https://www.rockauto.com/en/catalog/"
@@ -106,6 +107,10 @@ part_categories_href = list_stripper(results, list_position)
 # Testing for single element in the list. Convert to iteration when done
 
 ## This part gets a bit tricky and I'm unable to use list_stripper because there is no one properly defined class here. It goes from 'navlabellink nvoffset nimportant' to 'navlabellink nvoffset nnormal' to even 'navlabellink nvoffset nreversevideo'(lol wut?). So I tried a different approach here where I extract the link first and build the string from there.
+
+#Creating a master dataframe
+df =pd.DataFrame()
+
 part_cat = 'Accessories'
 inter_url = bare_url + part_categories_href['Accessories']
 page = requests.get(inter_url)
@@ -126,26 +131,57 @@ for href in href_list:
 for pc in pc_href:
 	print(pc)
 	inter_url = bare_url + pc_href[pc]
-	#print(inter_url)
 	page = requests.get(inter_url)
 	soup = BeautifulSoup(page.content, "html.parser")
+	#Getting the manufacturer details as well as the details of the part
 	results = soup.find_all("div",class_ ="listing-text-row-moreinfo-truck")
 	#print(type(result))
+
+	#Initializing Empty Lists
+	manufacturer_list = []
+	part_num_list = []
+	desc_list = []
+	part_text_list = []
+	price_list = []
+	
 	for result in results:
-		#print(result)
+		#The name of the manufacturer	
 		manufacturer = result.find("span", class_ = "listing-final-manufacturer")
 		print(manufacturer.get_text())
+		manufacturer_list.append(manufacturer.get_text())
+		#The part number in RockAuto
 		part_num = result.find("span", class_ = "listing-final-partnumber as-link-if-js buyers-guide-color")
 		print(part_num.get_text())
+		part_num_list.append(part_num.get_text())
+		#The description of the part
 		desc = result.find("span", class_ = "span-link-underline-remover")
+		desc_list.append(desc.get_text())
 		print(desc.get_text())
+	#Since the text uder the part description is in another class, it lies outside the listing-text-row-moreinfo-truck class
 	results_text = soup.find_all("div", class_ = "listing-text-row")
 	for result_text in results_text:
 		print(result_text.get_text())
-
+		part_text_list.append(result_text.get_text())
+	#Getting the price of the product
 	results_price = soup.find_all("span", class_ ="ra-formatted-amount listing-price listing-amount-bold")
 	for result_price in results_price:
 		print(result_price.get_text())
+		price_list.append(result_price.get_text())
+
+	print(part_num_list)
+	print(manufacturer_list)
+	print(desc_list)
+	print(part_text_list)
+	print(price_list)
+
+	#Creating an intermediate dataframe
+	df1= pd.DataFrame([part_num_list,manufacturer_list,desc_list, part_text_list,price_list]).T#, columns = ["ID","Manufacturer","Description","Details","Price"])
+	print(df1)
+	df = pd.concat([df,df1])
+
+df = df.rename(columns = {0:"ID",1:"Manufacturer",2:"Description",3:"Details",4:"Price"})	
+print(df)
+
 #<span class="listing-final-partnumber  as-link-if-js buyers-guide-color" id="vew_partnumber[10394]" onclick="if (cataloglite.IsMobileAndNotExpanded(&quot;10394&quot;)) { return; } cataloglite.ShowBuyersGuidePopup(&quot;10394&quot;);" title="Buyer's Guide" alt="Buyer's Guide">11423</span>
 	#kyu = result.find("span", class_ = "listing-final-manufacturer")
 #	print(kyu)
